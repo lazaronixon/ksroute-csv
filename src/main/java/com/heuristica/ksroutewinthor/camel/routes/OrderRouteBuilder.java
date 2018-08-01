820c0bd0133075d676a104187bf8c5782f3b8d36
Lázaro Nixon <lazaronixon@hotmail.com>
2018-07-31 21:02:57 -0300

    More work

--- src/main/java/com/heuristica/ksroutewinthor/camel/routes/OrderRouteBuilder.java
+++ src/main/java/com/heuristica/ksroutewinthor/camel/routes/OrderRouteBuilder.java
@@ -1,9 +1,13 @@
 package com.heuristica.ksroutewinthor.camel.routes;
 
 import com.heuristica.ksroutewinthor.LocalizedBindyDataFormat;
+import com.heuristica.ksroutewinthor.apis.OrderApi;
 import com.heuristica.ksroutewinthor.models.order.Branch;
 import com.heuristica.ksroutewinthor.models.order.Customer;
 import com.heuristica.ksroutewinthor.models.order.Order;
+import java.util.List;
+import org.apache.camel.component.jackson.ListJacksonDataFormat;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.util.toolbox.AggregationStrategies;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +17,8 @@ class OrderRouteBuilder extends ApplicationRouteBuilder {
     @Override
     public void configure() {
         super.configure();
+        
+        ListJacksonDataFormat jsonListDataformat = new ListJacksonDataFormat(OrderApi.class);        
 
         from("file://files/orders?move=done").routeId("process-order-file")
                 .log(">>>>>>> Inicio arquivo ${file:path}")
@@ -26,10 +32,37 @@ class OrderRouteBuilder extends ApplicationRouteBuilder {
                 .log(">>>> Inicio pedido ${body.erpId}")
                 .enrich("direct:process-branch", AggregationStrategies.bean(OrderEnricher.class, "setBranch"))
                 .enrich("direct:process-customer", AggregationStrategies.bean(OrderEnricher.class, "setCustomer"))
+                .convertBodyTo(OrderApi.class)
+                .enrich("direct:find-order", AggregationStrategies.bean(OrderEnricher.class, "setId"))
+                .choice().when(simple("${body.id} == null")).to("direct:create-order")
+                .otherwise().to("direct:update-order")               
                 .log(">>>> Fim pedido ${body.erpId}");
+        
+        from("direct:find-order").routeId("find-order")
+                .setHeader("CamelHttpMethod", constant("GET"))                               
+                .setHeader("Content-Type", constant("application/json"))
+                .setHeader("CamelHttpQuery", simple("q[erp_id_eq]=${body.erpId}"))
+                .throttle(5).setBody(constant("")).to("https4://{{ksroute.api.url}}/orders.json")
+                .unmarshal(jsonListDataformat);
+
+        from("direct:create-order").routeId("create-order")
+                .setHeader("CamelHttpMethod", constant("POST"))
+                .marshal().json(JsonLibrary.Jackson)
+                .throttle(5).to("https4://{{ksroute.api.url}}/orders.json");
+
+        from("direct:update-order").routeId("update-order")
+                .setHeader("CamelHttpMethod", constant("PUT"))               
+                .setHeader("orderId", simple("body.id"))
+                .marshal().json(JsonLibrary.Jackson)
+                .throttle(5).recipientList(simple("https4://{{ksroute.api.url}}/lines/${header.orderId}.json"));          
     }
     
     public class OrderEnricher {
+        
+        public OrderApi setId(OrderApi local, List<OrderApi> remoteList) {
+            local.setId(remoteList.isEmpty() ? local.getId() : remoteList.get(0).getId());
+            return local;
+        }        
 
         public Order setBranch(Order order, Branch branch) {
             order.setBranch(branch);
--- src/main/java/com/heuristica/ksroutewinthor/dozer/mappings/OrderMapping.java
+++ src/main/java/com/heuristica/ksroutewinthor/dozer/mappings/OrderMapping.java
@@ -3,12 +3,15 @@ package com.heuristica.ksroutewinthor.dozer.mappings;
 import com.heuristica.ksroutewinthor.apis.OrderApi;
 import com.heuristica.ksroutewinthor.models.order.Order;
 import org.dozer.loader.api.BeanMappingBuilder;
+import static org.dozer.loader.api.FieldsMappingOptions.copyByReference;
 
 public class OrderMapping extends BeanMappingBuilder {
 
     @Override
     protected void configure() {
-        mapping(Order.class, OrderApi.class);
+        mapping(Order.class, OrderApi.class)
+                .fields("issuedAt", "issuedAt", copyByReference())
+                .fields("customer.id", "customerId");
     }
 
 }
